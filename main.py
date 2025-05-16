477ec806746ff76ef6de4e7b5bed6e6de41fecf0
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-05-16 13:44:20 -0400

    Implement player movement, and drawable and updatable groups

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ from player import Player
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
+        # Check for exit event.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
